f04b59bd59c37d02143f76f085c6166b681eef17
Igor Suleymanoff <igorsuleymanoff@gmail.com>
2015-05-27 10:47:33 +0300

    Fix tabulation in DUT

--- ex6/ex6_dut.svh
+++ ex6/ex6_dut.svh
@@ -12,11 +12,10 @@ module ex6_dut(ex6_interface.dut_mp dut_interface);
 	    	SUB: dut_interface.result = dut_interface.value1 - dut_interface.value2;
 	    	MUL: dut_interface.result = dut_interface.value1 * dut_interface.value2;
 	    	DIV: if (dut_interface.value2 == 0) begin
-          dut_interface.result = 0;
-          dut_interface.correct = 0;
-        end
-        else
-          dut_interface.result = dut_interface.value1 / dut_interface.value2;
+	    		dut_interface.result = 0;
+	    		dut_interface.correct = 0;
+	    	     end
+	    	     else dut_interface.result = dut_interface.value1 / dut_interface.value2;
 
     $display("DUT @ %0t result = %p, correct = %p", $time, dut_interface.result, dut_interface.correct);
   end
